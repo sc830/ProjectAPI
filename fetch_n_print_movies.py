09694999e0d0a91ab77fe1a7a767e4a2a66be63f
Shelby Cumings <s.rs.cumings@gmail.com>
2023-10-21 21:37:39 -0500

    Prints part 1
    Unsorted array of trending media: names, popularity, and vote_count

--- fetch_n_print_movies.py
+++ fetch_n_print_movies.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())  # This is to load your API keys from .env
 
-TMDB_TRENDING_PATH = 'trending/movie/week'
+TMDB_TRENDING_PATH = 'trending/all/day'
 TMDB_SEARCH_API_REQUEST = f'https://api.themoviedb.org/3/{TMDB_TRENDING_PATH}?'
 
 #def get_top_10_weekly_trending_movies():
@@ -17,13 +17,31 @@ response = requests.get(
 )
 # Encodes response into a python json dictionary.
 json_data = response.json()
-print(json_data)
+#print(json_data)
 
 # Convert json_data to a formatted pretty
 # json string that is easy for humans to read.
 # Mouse over function to get definition of indent and sort_keys
 pretty_json_data = json.dumps(json_data, indent=4, sort_keys=True)
-print(pretty_json_data)
+#print(pretty_json_data)
 
-weekly_trending_movie_object = json_data
+daily_trending = json_data
 # Add Parsing Code Here
+
+#movies = [item for item in daily_trending['results'] if item['media_type'] == 'movie']
+media = []
+
+for item in daily_trending['results']:
+    if item['media_type'] == 'movie':
+        new = {'title': item['title'], 'popularity': item['popularity'], 'vote_count': item['vote_count']}
+        media.append(new)
+    else:
+        new = {'title': item['name'], 'popularity': item['popularity'], 'vote_count': item['vote_count']}
+        media.append(new)
+
+for item in media:
+    print(f"Title/Name: {item['title']:<50}Popularity: {item['popularity']:<30}Vote Count: {item['vote_count']:.1f}")
+
+        
+
+        
